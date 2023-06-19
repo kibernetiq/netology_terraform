2bc3a85a4f0e38d150688705d83905da0b7b45ae
Yura <yura@Skynet.local>
2023-06-19 23:31:39 +0300

    Домашнее задание к занятию 'Использование Terraform в команде'

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "s3" {
+  endpoint = "storage.yandexcloud.net"
+  bucket   = "tfstate-object"
+  region   = "ru-central1-a"
+  key      = "terraform.tfstate"
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  dynamodb_endpoint           = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g3kjd2qonru87vca0q/etn0kkc08dpq2u63kual"
+  dynamodb_table              = "tfstate-table"
+}
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
